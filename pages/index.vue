b91e83a1dc0e9cc8e240509ac68ea2b33fd74a99
John Châtel <john.chatel@digitalstories.com>
2024-05-06 20:50:02 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex h-screen w-screen items-center justify-items-center justify-center" >
+    <h1 class="text-4xl font-semibold">Welcome to simple-page!</h1>
+  </div>
+</template>
